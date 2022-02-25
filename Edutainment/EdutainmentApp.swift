13bc4964a881183aad85d50c178ac3b0476d46c1
YossiBenZaken <yossibz@gmail.com>
2022-02-25 10:06:30 +0200

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Yosef Ben Zaken on 25/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
